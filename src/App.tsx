63812d5733ee02027140740ccde5c4c68b4adc73
Fede <federicoalvarez710@gmail.com>
2021-05-31 17:19:39 -0300

    add some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import React from "react";
-import { Card, TransactionCard } from "./lib/components";
-import { Body } from "./styles";
-import { Dashboard } from "./containers/dashboard";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import {Card, TransactionCard} from './lib/components';
+import {Dashboard} from './containers/dashboard';
+import {Deposit} from './containers/deposit';
+import {Body} from './styles';
 
 function App() {
   return (
-    <Body>
-      <div style={{ width: "80%" }} className="row g-0 p-5 ">
-        <Dashboard />
-      </div>
-    </Body>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Body>
+            <div style={{width: '80%'}} className="row g-0 p-5 ">
+              <Dashboard />
+            </div>
+          </Body>
+        </Route>
+        <Route path="/deposit">
+          <Body>
+            <div style={{width: '80%'}} className="row g-0 p-5 ">
+              <Deposit />
+            </div>
+          </Body>
+        </Route>
+        <Route path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
